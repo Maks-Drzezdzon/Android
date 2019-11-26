018aa6cc6acec931cdb596d5f3e3207327d7005c
maksthorn <maksdrzezdzon@gmail.com>
2019-11-26 23:35:09 +0000

    starting music app

---
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/CreateNotification.java
@@ -0,0 +1,27 @@
+package com.example.musicplayer;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+
+import androidx.core.app.NotificationManagerCompat;
+
+public class CreateNotification {
+    public static final String CHANNEL_ID = "channel1";
+
+    public static final String ACTIONPREVIOUS = "actionprevious";
+    public static final String CHANNEL_PLAY = "actionplay";
+    public static final String CHANNEL_NEXT = "actionnext";
+
+    public static Notification notification;
+
+    public static void createNotificaiton(Context context, Track track, int playbutton, int pos, int size){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        }
+
+    }
+
+
+}
