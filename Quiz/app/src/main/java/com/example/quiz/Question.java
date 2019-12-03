d70fc6cebf71de0a1c502be118a77daa8d774794
maksthorn <maksdrzezdzon@gmail.com>
2019-12-03 19:06:38 +0000

    playing around with android after interviews

---
+++ Quiz/app/src/main/java/com/example/quiz/Question.java
@@ -0,0 +1,27 @@
+package com.example.quiz;
+
+public class Question {
+    private int answerId;
+    private boolean answerTrue;
+
+    public Question(int answerId, boolean answerTrue) {
+        this.answerId = answerId;
+        this.answerTrue = answerTrue;
+    }
+
+    public int getAnswerId() {
+        return answerId;
+    }
+
+    public void setAnswerId(int answerId) {
+        this.answerId = answerId;
+    }
+
+    public boolean isAnswerTrue() {
+        return answerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        this.answerTrue = answerTrue;
+    }
+}
