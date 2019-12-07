522a9a323993010701822d3827ca6780f17fefe6
maksthorn <maksdrzezdzon@gmail.com>
2019-12-07 22:55:35 +0000

    done for today

--- TinderClone/app/src/main/java/com/example/tinderclone/RegistrationActivity.java
+++ TinderClone/app/src/main/java/com/example/tinderclone/RegistrationActivity.java
@@ -3,12 +3,29 @@ package com.example.tinderclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private Button mRegister;
+    private EditText mEmail, mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        mRegister = findViewById(R.id.register);
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do things
+            }
+        });
     }
 }
